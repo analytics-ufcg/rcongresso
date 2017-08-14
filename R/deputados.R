674b47926299885dfb7aca300ab11c8186e8ffce
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-14 11:11:26 -0300

    Add get_deputy() function
    Get deputy's details by passing its id and returns a list containing all the information.

---
+++ R/deputados.R
@@ -0,0 +1,16 @@
+# Get deputy's details
+# dep_id: Deputy's Register ID
+#   ideCadastro param get from http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/deputados/obterdeputados
+# Return: List containing the details about the deputy
+get_deputy <- function(dep_id){
+
+  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/deputados/", dep_id, sep="")
+
+  dep <- GET(full_link)
+  r <- content(dep, as="text")
+  dep_json <- jsonlite::fromJSON(r)
+  dep_list <- dep_json$dados
+
+  return(dep_list)
+
+}
