cbf417470d849c0b501347ff201d4dcd7c3548ab
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-14 10:40:15 -0300

    Finish get_all_propositions() function
    Get all the propositions available on the database and return a dataframe.

--- R/proposicoes.R
+++ R/proposicoes.R
@@ -1,11 +1,37 @@
-get_all_proposicoes <- function(){
+# This function returns all the propositions available on Camara's site
+get_all_propositions <- function(){
 
-  # Here's gonna have a loop or something to fetch all the propositions available in every page.
-  # The GET command recovers only 100 items for request.
   propositions <- GET("https://dadosabertos.camara.leg.br/api/v2/proposicoes?ordem=ASC&ordenarPor=id&itens=100")
   raise <- content(propositions, as="text")
   propositions_json <- jsonlite::fromJSON(raise)
   propositions_dataframe <- propositions_json$dados
-  propositions_references <- propositions_json$links
+
+  next_page <- propositions_json$links$href[2]
+  self_page <- propositions_json$links$href[3]
+  first_page <- propositions_json$links$href[3]
+  last_page <- propositions_json$links$href[4]
+
+  # Loop to get all the available propositions. It will take longer than 15 minutes to get all of
+  # them. Nowdays, the site has more than 6K pages to go through (14/08/2017).
+  while(TRUE){
+    p <- GET(next_page)
+    r <- content(p, as="text")
+    p_json <- jsonlite::fromJSON(r)
+    p_dataframe <- p_json$dados
+
+    propositions_dataframe <- rbind(propositions_dataframe, p_dataframe)
+
+    self_page <- next_page
+    next_page <- p_json$links$href[2]
+
+    print(self_page)
+
+    if(next_page == first_page) {
+      break;
+    }
+
+  }
+
+  return(propositions_dataframe)
 
 }
