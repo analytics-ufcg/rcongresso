29c20b87378589f4796943ad25f32a0246e0f618
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-14 12:18:24 -0300

    Add get_voters() function
    Gets votes of every deputy about a voting.
    *Not ready yet. It's getting only the 100 first deputy's votes.*

--- R/votacoes.R
+++ R/votacoes.R
@@ -12,3 +12,20 @@ get_voting <- function(id_vot){
 
   return(voting_list)
 }
+
+# Gets votes of every deputy about a voting
+# id_vot: Voting's ID
+# Return: Dataframe containing the data/vote of every deputy.
+get_voters <- function(id_vot){
+
+  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/votacoes/", id_vot, "/votos?itens=513", sep="")
+
+  voters <- GET(full_link)
+  r <- content(voters, as="text")
+  voters_json <- jsonlite::fromJSON(r)
+  voters_dataframe <- voters_json$dados
+
+  return(voters_dataframe)
+}
+
+
