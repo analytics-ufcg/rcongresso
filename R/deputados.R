f1af32a6851b98e07a01c621bc30889fe6f1b194
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-14 11:24:10 -0300

    Add get_deputy_expenses() function
    Gets all deputy's expenses

--- R/deputados.R
+++ R/deputados.R
@@ -1,4 +1,4 @@
-# Get deputy's details
+# Gets deputy's details
 # dep_id: Deputy's Register ID
 #   ideCadastro param get from http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/deputados/obterdeputados
 # Return: List containing the details about the deputy
@@ -12,5 +12,23 @@ get_deputy <- function(dep_id){
   dep_list <- dep_json$dados
 
   return(dep_list)
+
+}
+
+
+# Gets all deputy's expenses
+# dep_id: Deputy's Register ID
+#   ideCadastro param get from http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/deputados/obterdeputados
+# Return: Dataframe containing the details about the deputy's expenses
+get_deputy_expenses <- function(dep_id) {
+
+  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/deputados/", dep_id, "/despesas?ordem=ASC&ordenarPor=numAno", sep="")
+
+  dep <- GET(full_link)
+  r <- content(dep, as="text")
+  dep_json <- jsonlite::fromJSON(r)
+  dep_dataframe <- dep_json$dados
+
+  return(dep_dataframe)
 
 }
