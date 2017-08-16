aef46fd21a8cd855842f42b95db1457248f93ed0
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-16 17:47:34 -0300

    Adiciona função constroi_dataframe(proposicao, votacao, votantes)
    Ao receber esses três parâmetros, a função retorna um dataframe contendo colunas com as informações: nomeVotantes, idParlamentar, siglaPartido, siglaUFPartido, votos, proposicaoVotada, anoProposicao, ementaProposicao e dataVotacao.

---
+++ R/qmr.R
@@ -0,0 +1,39 @@
+# Dúvida: Quais deverão ser os parâmetros dessa função?
+# 1: Três parâmetros: Uma proposição, um 'objeto' votação e um 'objeto' votantes (Dessa maneira, a responsabilidade de fazer
+# um get desses três 'objetos' é responsabilidade de quem vai chamar a função)
+# 2: Quatro parâmetros: O tipo da proposição, o número da proposição e o ano da proposição e o ID
+# da votação que eu quero (Dessa maneira, o método faria uma busca automática pela proposição e
+# o ID da votação seria responsabilidade de quem está chamando o método, o que não faz muito sentido
+# porque se ele tem o ID da votação ele deverá ter o ID da proposição também e não facilita muito.)
+# 3: Quatro parâmetros: O tipo da proposição, o número da proposição e o ano da proposição e
+# a data da votação (Acho que essa seria a maneira ideal e mais amigável para o usuário. Ele passa
+# os dados referentes à proposição a à votação e aí tem tudo junto daquela votação especificamente.
+# Temos que lidar com o caso que há mais de uma votação no mesmo dia e o parse da data.)
+
+# Para facilitar vou escolhar a opção 1 e testar a construção do dataframe. Depois eu posso mudar.
+constroi_dataframe <- function(proposicao, votacao, votantes) {
+
+  prop_types <- rcongresso::fetch_tipo_proposicao()
+  p <- prop_types %>% filter(prop_types$id==proposicao$idTipo)
+
+  dataframe_final <- data.frame()
+
+  # Quero gerar um for para pegar as colunas a partir de uma lista ao invés de fazer dessa forma.
+  # A variável de controle do for seria o parametro votantes$...
+  dataframe_final <- rbind(dataframe_final, data.frame(votantes$parlamentar$nome))
+  dataframe_final <- cbind(dataframe_final, data.frame(votantes$parlamentar$id))
+  dataframe_final <- cbind(dataframe_final, data.frame(votantes$parlamentar$siglaPartido))
+  dataframe_final <- cbind(dataframe_final, data.frame(votantes$parlamentar$siglaUf))
+  dataframe_final <- cbind(dataframe_final, data.frame(votantes$voto))
+
+  dataframe_final <- cbind(dataframe_final, data.frame(proposicao$numero))
+  dataframe_final <- cbind(dataframe_final, data.frame(proposicao$ano))
+  dataframe_final <- cbind(dataframe_final, data.frame(proposicao$ementa))
+
+  dataframe_final <- cbind(dataframe_final, data.frame(votacao$dataHoraFim))
+
+  # Antes de retornar o dataframe seria bom renomear as colunas. Eu vou fazer isso após conseguir
+  # pegar todos os dados que são necessários. Ainda quero confirmar se peguei tudo mesmo.
+return(dataframe_final)
+
+}
