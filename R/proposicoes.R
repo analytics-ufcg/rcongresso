b0b79eb2d476f942ef51f796539440939f6ad383
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-22 12:40:00 -0300

    Adiciona documentação em proposicoes.R

--- R/proposicoes.R
+++ R/proposicoes.R
@@ -1,5 +1,11 @@
-# Função que retorna todas as proposições disponíveis na base de dados da Câmara dos Deputados
-# Return: Dataframe contendo todas as informações das proposições.
+#' Recupera da API todas as proposições disponíveis na base de dados da Câmara dos Deputados.
+#'
+#' @return Dataframe contendo todas as informações das proposições
+#'
+#' @examples
+#' proposicoes <- fetch_proposicoes()
+#'
+#' @export
 fetch_proposicoes <- function(){
 
   props_link <- "https://dadosabertos.camara.leg.br/api/v2/proposicoes?ordem=ASC&ordenarPor=id&itens=100"
@@ -34,9 +40,16 @@ fetch_proposicoes <- function(){
 
 }
 
-# Recupera uma proposição contendo detalhes adicionais.
-# id_prop: ID da proposição.
-# Return: Lista contendo informações sobre a proposição.
+#' Recupera da API uma proposição contendo detalhes adicionais.
+#'
+#' @param id_prop ID da proposição
+#'
+#' @return Lista contendo informações sobre a proposição
+#'
+#' @examples
+#' pec241 <- fecth_proposicao(2088351)
+#'
+#' @export
 fetch_proposicao <- function(id_prop){
 
   full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/proposicoes/", id_prop, sep="")
@@ -47,12 +60,19 @@ fetch_proposicao <- function(id_prop){
 
 }
 
-# Recupera todas as votações por quais uma proposição já passou.
-# id_prop: ID da proposição.
-# Return: Dataframe contendo as várias votações pelas quais uma proposição passou.
-fetch_votacoes <- function(proposicao){
+#' Recupera da API todas as votações pelas quais uma proposição já passou.
+#'
+#' @param id_prop ID da proposição
+#'
+#' @return Dataframe contendo as várias votações pelas quais uma proposição passou.
+#'
+#' @examples
+#' votacoes_pec241 <- fetch_votacoes(2088351)
+#'
+#' @export
+fetch_votacoes <- function(id_prop){
 
-  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/proposicoes/", proposicao, "/votacoes", sep="")
+  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/proposicoes/", id_prop, "/votacoes", sep="")
 
   voting_json <- .get_json(full_link)
 
@@ -60,6 +80,19 @@ fetch_votacoes <- function(proposicao){
 
 }
 
+#' Recupera da API o ID da preposição de acordo com o tipo, número da proposição e ano que
+#' esta foi feita.
+#'
+#' @param tipo Tipo da proposição (i.e., PEC, PL, PDC)
+#' @param numero Número da proposição
+#' @param ano Ano da proposição
+#'
+#' @return ID da proposição
+#'
+#' @examples
+#' pec241_id <- fetch_id_proposicao("PEC", 241, 2016)
+#'
+#' @export
 fetch_id_proposicao <- function(tipo, numero, ano){
 
   full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/proposicoes?siglaTipo=", tipo, "&numero=", numero, "&ano=", ano,"&ordem=ASC&ordenarPor=id", sep="")
@@ -70,6 +103,14 @@ fetch_id_proposicao <- function(tipo, numero, ano){
   return(prop_json$dados$id)
 }
 
+#' Recupera da API os tipos de proposição disponíveis.
+#'
+#' @return Tipos de proposição
+#'
+#' @examples
+#' tipos_proposicao <- fetch_tipos_proposicao()
+#'
+#' @export
 fetch_tipos_proposicao <- function(){
   prop_types_link <- "https://dadosabertos.camara.leg.br/api/v2/referencias/tiposProposicao"
 
