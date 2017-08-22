8ba4c436fa096c22b6782c0e4cd7f013c0c5f00a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-22 12:38:47 -0300

    Adiciona documentação em deputados.R

--- R/deputados.R
+++ R/deputados.R
@@ -1,7 +1,13 @@
-# Recupera os detalhes de um deputado
-# dep_id: ID do Deputado
-#   ideCadastro param get from http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/deputados/obterdeputados
-# Return: Lista contendo detalhes sobre o deputado.
+#' Recupera da API os detalhes de um deputado.
+#'
+#' @param dep_id ID do Deputado
+#'
+#' @return Lista contendo detalhes sobre o deputado.
+#'
+#' @examples
+#' abel_mesquita_info <- fetch_deputado(178957)
+#'
+#' @export
 fetch_deputado <- function(dep_id){
 
   full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/deputados/", dep_id, sep="")
@@ -12,11 +18,16 @@ fetch_deputado <- function(dep_id){
 
 }
 
-
-# Recupera todos os gastos de um deputado específico.
-# dep_id: ID do deputado (ideCadastro)
-#   ideCadastro param get from http://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo/webservices/deputados/obterdeputados
-# Return: Dataframe contendo detalhes sobre os gastos do deputado.
+#' Recupera todos os gastos da cota parlamentar, dos últimos seis meses, um deputado específico.
+#'
+#' @param dep_id ID do deputado
+#'
+#' @return Dataframe contendo detalhes sobre os gastos do deputado
+#'
+#' @examples
+#' gastos_abel_mesquita <- fetch_despesas_deputado(178957)
+#'
+#' @export
 fetch_despesas_deputado <- function(dep_id) {
 
   full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/deputados/", dep_id, "/despesas?ordem=ASC&ordenarPor=numAno", sep="")
