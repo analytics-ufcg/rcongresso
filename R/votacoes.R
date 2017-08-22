8237246ad4a81099b69e3c0a35d27008c3cf15a7
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-22 12:42:05 -0300

    Adiciona documentação em votacoes.R
    Além disso, muda parâmetros que eram 'votacao' para 'id_votacao' a fim de deixar mais compreensível para o usuário final.

--- R/votacoes.R
+++ R/votacoes.R
@@ -1,25 +1,38 @@
-# Recupera detalhes sobre uma votação específica pelo seu ID.
-# id_vot: ID da votação.
-# Return: Lista contendo os detalhes de uma votação, incluindo o posicionamento de cada partido.
-fetch_votacao <- function(votacao){
+#' Recupera da API detalhes sobre uma votação específica.
+#'
+#' @param id_votacao ID da votação
+#'
+#' @return Lista contendo os detalhes de uma votação, incluindo o posicionamento de cada bancada
+#'
+#' @examples
+#' votacao_segundoturno_pec241 <- fetch_votacao(7252)
+#'
+#' @export
+fetch_votacao <- function(id_votacao){
 
-  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/votacoes/", votacao, sep="")
+  full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/votacoes/", id_votacao, sep="")
 
   votacao_json <- .get_json(full_link)
 
   return(votacao_json$dados)
 }
 
-# Recupera os votantes referentes àquela votação específica
-# id_vot: ID da votação.
-# Return: Dataframe contendo o posicionamento de cada deputado, além de informações sobre estes.
-fetch_votos <- function(votacao){
+#' Recupera os votos referentes àquela votação específica.
+#' @param id_votacao ID da votação
+#'
+#' @return Dataframe contendo o posicionamento de cada deputado, além de informações sobre estes.
+#'
+#' @examples
+#' votos_segundoturno_pec241 <- fetch_votos(7252)
+#'
+#' @export
+fetch_votos <- function(id_votacao){
 
   votantes_dataframe <- data.frame()
 
   for(i in 1:5){
 
-    full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/votacoes/", votacao, "/votos?pagina=", i,"&itens=513", sep="")
+    full_link <- paste("https://dadosabertos.camara.leg.br/api/v2/votacoes/", id_votacao, "/votos?pagina=", i,"&itens=513", sep="")
 
     print(full_link)
 
