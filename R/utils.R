68a0e174b5aad1f0c3f9cdc3823784c062d30951
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-22 12:40:25 -0300

    Adiciona documentação em utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,13 @@
+#' Função que recupera um json a partir de um link que use o protocolo REST, desde que esse permita a conversão.
+#'
+#' @param full_link Link para a solicitação GET
+#'
+#' @return Json contendo as informações requeridas
+#'
+#' @examples
+#' pec241_json <- .get_json("https://dadosabertos.camara.leg.br/api/v2/proposicoes/2088351")
+#'
+#' @export
 .get_json <- function(full_link){
 
   prop <- httr::GET(full_link)
