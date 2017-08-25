e0c02795379143344e5cab16892bf77b26bdd55c
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-25 16:19:41 -0300

    Renomeia testes para auxiliar nas mensagens informativas de erro

--- tests/testthat/test_proposicoes.R
+++ tests/testthat/test_proposicoes.R
@@ -1,32 +1,32 @@
 # Verifica se o ID da PEC241 retornado está correto.
 pec_241_id <- fetch_id_proposicao(tipo = "PEC", numero = 241, ano = 2016)
-test_that("Mensagem default", {expect_equal(pec_241_id, 2088351)})
+test_that("ID PEC241", {expect_equal(pec_241_id, 2088351)})
 
 # Verifica se o ID da PEC287 retornado está correto.
 pec_287_id <- fetch_id_proposicao(tipo = "PEC", numero = 287, ano = 2016)
-test_that("Mensagem default", {expect_equal(pec_287_id, 2119881)})
+test_that("ID PEC287", {expect_equal(pec_287_id, 2119881)})
 
 # Verifica se o ID da PEC22 retornado está correto.
 pec_22_id <- fetch_id_proposicao(tipo = "PEC", numero = 22, ano = 2011)
-test_that("Mensagem default", {expect_equal(pec_22_id, 500843)})
+test_that("ID PEC22", {expect_equal(pec_22_id, 500843)})
 
 # Verifica se as informações relacionadas às proposições são pertinentes.
 # Existe um idTipo que identifica os tipos de proposição em tramitação/votação na
 #    Câmara. Nesse caso, 136 é PEC.
 pec_241 <- fetch_proposicao(pec_241_id)
-test_that("Mensagem default", {expect_equal(pec_241$idTipo, 136)})
-test_that("Mensagem default", {expect_equal(pec_241$numero, 241)})
-test_that("Mensagem default", {expect_equal(pec_241$ano, 2016)})
+test_that("Tipo PEC241=='PEC'", {expect_equal(pec_241$idTipo, 136)})
+test_that("Numero PEC241", {expect_equal(pec_241$numero, 241)})
+test_that("Ano PEC241", {expect_equal(pec_241$ano, 2016)})
 
 pec_287 <- fetch_proposicao(pec_287_id)
-test_that("Mensagem default", {expect_equal(pec_287$idTipo, 136)})
-test_that("Mensagem default", {expect_equal(pec_287$numero, 287)})
-test_that("Mensagem default", {expect_equal(pec_287$ano, 2016)})
+test_that("Tipo PEC287=='PEC'", {expect_equal(pec_287$idTipo, 136)})
+test_that("Numero PEC287", {expect_equal(pec_287$numero, 287)})
+test_that("Ano PEC287", {expect_equal(pec_287$ano, 2016)})
 
 pec_22 <- fetch_proposicao(pec_22_id)
-test_that("Mensagem default", {expect_equal(pec_22$idTipo, 136)})
-test_that("Mensagem default", {expect_equal(pec_22$numero, 22)})
-test_that("Mensagem default", {expect_equal(pec_22$ano, 2011)})
+test_that("Tipo PEC22=='PEC'", {expect_equal(pec_22$idTipo, 136)})
+test_that("Numero PEC22", {expect_equal(pec_22$numero, 22)})
+test_that("Ano PEC22", {expect_equal(pec_22$ano, 2011)})
 
 # Verifica se as informações relacionadas às votações são pertinentes.
 votacoes_pec_241 <- fetch_votacoes(pec_241_id)
@@ -37,6 +37,6 @@ ids <- c(7214, 7252, 7207, 7209, 7206, 7223, 7221, 7218, 7222, 7208, 7210,
 
 ids_votacoes_pec241 <- votacoes_pec_241$id
 r <- compare::compareEqual(ids, ids_votacoes_pec241)
-test_that("Mensagem default", {expect_true(r$result)})
+test_that("IDs Votacoes PEC241", {expect_true(r$result)})
 
 
