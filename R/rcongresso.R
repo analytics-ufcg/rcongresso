3efada44066da2019d23906430cd8167c18cbc08
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-25 12:22:45 -0300

    Adiciona arquivo raiz do pacote
    Serve para imports no futuro e documentação do pacote.
    Bugfix #5

---
+++ R/rcongresso.R
@@ -0,0 +1,9 @@
+#' rcongresso: Pacote R para acessar dados do congresso nacional.
+#'
+#' O pacote dá acesso à várias funções que são divididas em categorias: Proposições, deputados e votações.
+#' Veja mais detalhes nas funções ou consultando o site da câmara na referência abaixo.
+#'
+#' @docType package
+#' @name rcongresso
+#' @references \url{https://dadosabertos.camara.leg.br/swagger/api.html}
+NULL
