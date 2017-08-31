70fee37a1026823843a3822ef50e2d8f3d5abb18
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-08-31 16:53:43 -0300

    Cria novas constantes para PATHS da API

--- R/constants.R
+++ R/constants.R
@@ -1,2 +1,11 @@
-.API_LINK <- "https://dadosabertos.camara.leg.br/api/v2/"
+# Link default da API
+.API_LINK <- "https://dadosabertos.camara.leg.br"
+
+# Mensagens de erro
 .ERRO_RETORNO_JSON <- "API did not return json"
+
+# Paths
+.PROPOSICOES_PATH <- "/api/v2/proposicoes"
+.VOTACOES_PATH <- "/api/v2/votacoes"
+.DEPUTADOS_PATH <- "/api/v2/deputados"
+.TIPOS_PROPOSICOES_PATH <- "/api/v2/referencias/tiposProposicao"
