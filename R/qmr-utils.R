beeadeac337bda7cc68bb84b423627297423171a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-09-05 12:15:57 -0300

    Testa as variações de regex para cobrir todos os casos
    Atualmente só um caso não é coberto: PtdoB no início/meio da frase.

---
+++ R/qmr-utils.R
@@ -0,0 +1,8 @@
+# regex tests
+# PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]])', ' \\1', x)
+# PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]])^(?!.*PtdoB).*$', ' \\1', x)
+# b <- unlist(strsplit(y, split=" "))
+
+#PT_SPLIT <- gsub('([[(?=.*PtdoB).*$]]|([[:upper:]]+[[:lower:]]))', ' \\1', x_modified)
+#PT_SPLIT <- gsub('([[^(?=.*PtdoB).*$]]|([[:upper:]]+[[:lower:]]))', ' \\1', x_modified)
+#PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]]|*.PtdoB))', ' \\1', x_modified)
