fef7806e87e3c08b5ff6def2c8b9262a3e129d4a
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-09-13 11:30:42 -0300

    Simplifica função .get_votos_partidos
    Regex precisa ser revisto. Surgiram novos casos a serem cobertos.

--- R/qmr-utils.R
+++ R/qmr-utils.R
@@ -1,33 +1,13 @@
-# regex tests
-# PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]])', ' \\1', x)
-# PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]])^(?!.*PtdoB).*$', ' \\1', x)
-# b <- unlist(strsplit(y, split=" "))
-
-# PT_SPLIT <- gsub('([[(?=.*PtdoB).*$]]|([[:upper:]]+[[:lower:]]))', ' \\1', x_modified)
-# PT_SPLIT <- gsub('([[^(?=.*PtdoB).*$]]|([[:upper:]]+[[:lower:]]))', ' \\1', x_modified)
-# PT_SPLIT <- gsub('([[:upper:]]+[[:lower:]]|*.PtdoB))', ' \\1', x_modified)
-
+# regex quebra para casos de GOV. e PCdoB.
 .get_votos_partidos <- function(votacao) {
-  pos_bancadas <- votacao$orientacoes %>% dplyr::select(nomeBancada, voto)
 
-  # Eu estava pensando em um jeito de separar os nomes dos partidos, mas sempre ao fazer isso perdemos a referência
-  # para os votos porque "quebramos" o voto da bancada em muitos partidos. A solução temporária que encontrei foi essa:
-  # Ao passar por cada bancada, antes de splitar, eu associo a bancada à todos os partidos filhos daquela bancada.
-  # Dessa forma, posso recuperar o voto para cada partido fazendo um join pelo nome da bancada.
-  partidos_df <- data.frame()
-  for(bancada in pos_bancadas$nomeBancada){
-    split_partidos <- gsub('([[(?=.*PtdoB).*$]]|([[:upper:]]+[[:lower:]]))', ' \\1', bancada)
-    split_partidos <- gsub('Repr[.]*', ' \\1', split_partidos)
-    vetor_partidos <- unlist(strsplit(split_partidos, split=" "))
-    tmp <- data.frame(partido=vetor_partidos)
-    tmp$bancada_associada <- bancada
-    partidos_df <- rbind(partidos_df, tmp)
-  }
+  pos_bancadas <- votacao$orientacoes %>%
+    dplyr::mutate(bancada_associada=nomeBancada) %>%
+    dplyr::select(partido=nomeBancada, orientacao_partido=voto, bancada_associada) %>%
+    tidyr::separate_rows(partido, sep='(?=[A-Z][^A-Z])')
 
-  partidos_df <- dplyr::left_join(pos_bancadas, partidos_df, by=c("nomeBancada" = "bancada_associada"))
-  partidos_df$partido <- toupper(partidos_df$partido)
-  colnames(partidos_df)[2] <- "orientacao_partido"
+  # Queria colocar isso no pipe, mas ainda não consegui usar a sintaxe correta.
+  pos_bancadas$partido <- toupper(pos_bancadas$partido)
 
-  return(partidos_df)
-
+  return(pos_bancadas)
 }
