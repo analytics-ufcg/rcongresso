b8cb0f2504c731623bd9a8b12e30a43cf482c5f6
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-09-15 17:06:25 -0300

    Atualiza exemplo

--- examples/pega_votacoes.R
+++ examples/pega_votacoes.R
@@ -3,24 +3,33 @@ library(dplyr)
 library(readr)
 library(rcongresso)
 
-args = commandArgs(trailingOnly = TRUE)
-if (length(args) != 1) {
-  stop(
-    "Uso: pega_votacoes.R <arquivo_de_proposicoes>"
-  )
-}
+# args = commandArgs(trailingOnly = TRUE)
+# if (length(args) != 1) {
+#   stop(
+#     "Uso: pega_votacoes.R <arquivo_de_proposicoes>"
+#   )
+# }
+#
+# arquivo_proposicoes = args[1]
+arquivo_proposicoes = "arquivo_proposicoes.txt"
 
-arquivo_proposicoes = args[1]
-
-ids_proposicoes <- read_csv(arquivo_proposicoes) %>%
+ids_proposicoes <- arquivo_proposicoes  %>%
+  read_csv(col_types = cols(
+    tipo = col_character(),
+    numero = col_integer(),
+    ano = col_integer()
+  )) %>%
   rowwise() %>%
   do(tibble(id = fetch_id_proposicao(.$tipo, .$numero, .$ano)))
 
-votacoes_relevantes <- ids_proposicoes %>%
-  fetch_votacoes() %>%
-  group_by(proposicao) %>%
+
+proposicoes <- fetch_proposicao(ids_proposicoes$id)
+
+ultimas_votacoes <- fetch_votacoes(ids_proposicoes$id) %>%
   ultima_votacao()
 
-votacoes_relevantes %>%
+votacoes_relevantes <- fetch_votacao(ultimas_votacoes$id)
+
+constroi_dataframe(proposicoes, votacoes_relevantes) %>%
   format_csv() %>%
   writeLines(stdout())
