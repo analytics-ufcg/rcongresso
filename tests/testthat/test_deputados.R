6fff81ad89be72c58a9145a54801a4b87528ac90
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-09-27 12:08:18 -0300

    Adiciona e padroniza testes das funções dos deputados

--- tests/testthat/test_deputados.R
+++ tests/testthat/test_deputados.R
@@ -1,10 +1,50 @@
+# Testa erros
+test_that("GET deputado inexistente", {expect_error(fetch_deputado(22))})
+
+# Setup
+NOME_ESPERADO <- "ABEL SALVADOR MESQUITA JUNIOR"
 id_abel_mesquita <- 178957
 abel_mesquita_info <- fetch_deputado(id_abel_mesquita)
-NOME_ESPERADO <- "ABEL SALVADOR MESQUITA JUNIOR"
-
-test_that("Nome do deputado", {expect_equal(abel_mesquita_info$nomeCivil, NOME_ESPERADO)})
-test_that("Partido do deputado", {expect_equal(abel_mesquita_info$ultimoStatus$siglaPartido, "DEM")})
-test_that("UF do deputado", {expect_equal(abel_mesquita_info$ultimoStatus$siglaUf, "RR")})
-
 abel_mesquita_gastos <- fetch_despesas_deputado(id_abel_mesquita)
-test_that("Dataframe de despesas", {expect_true(is.data.frame(abel_mesquita_gastos))})
+
+colnames_abel_mesquita_info <- c("id","uri","nomeCivil","cpf","sexo","dataNascimento",
+                        "ufNascimento","municipioNascimento","escolaridade")
+colnames_abel_mesquita_gastos <- c("ano","mes","tipoDespesa","idDocumento","tipoDocumento",
+                                   "dataDocumento","numDocumento","valorDocumento",
+                                   "urlDocumento","nomeFornecedor","cnpjCpfFornecedor",
+                                   "valorLiquido","valorGlosa","numRessarcimento","idLote","parcela")
+
+tipos_abel_mesquita_info <- c("integer","character","character","character",
+                              "character","character","character","character",
+                              "character")
+tipos_abel_mesquita_gastos <- rep("character", 16)
+
+names(tipos_abel_mesquita_info) <- colnames_abel_mesquita_info
+names(tipos_abel_mesquita_gastos) <- colnames_abel_mesquita_gastos
+
+# Testes
+test_that("Is dataframe", {
+  expect_true(is.data.frame(abel_mesquita_info))
+  expect_true(is.data.frame(abel_mesquita_gastos))
+})
+
+test_that("Dimensoes do dataframe",{
+  expect_equal(dim(abel_mesquita_info), c(1, 9))
+  expect_equal(ncol(abel_mesquita_gastos), 16)
+})
+
+test_that("Atributos do dataframe",{
+  expect_equal(attributes(abel_mesquita_info)$names, colnames_abel_mesquita_info)
+  expect_equal(attributes(abel_mesquita_gastos)$names, colnames_abel_mesquita_gastos)
+})
+
+test_that("Campos do dataframe",{
+  expect_equal(sapply(abel_mesquita_info, class), tipos_abel_mesquita_info)
+  expect_equal(sapply(abel_mesquita_gastos, class), tipos_abel_mesquita_gastos)
+})
+
+test_that("ID do deputado", {expect_equal(abel_mesquita_info$id, 178957)})
+test_that("Nome do deputado", {expect_equal(abel_mesquita_info$nomeCivil, NOME_ESPERADO)})
+test_that("Data de nascimento do deputado", {expect_equal(abel_mesquita_info$dataNascimento, "1962-03-29")})
+test_that("UF de nascimento do deputado", {expect_equal(abel_mesquita_info$ufNascimento, "RR")})
+
