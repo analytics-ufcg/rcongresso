513b1d13d68b0e2e72a84687d8c9cbf1de6df88f
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-10-05 16:22:17 -0300

    Atualiza testes de 'status_proposicao'
    Um novo campo foi incluído no retorno.

--- tests/testthat/test_proposicoes.R
+++ tests/testthat/test_proposicoes.R
@@ -11,13 +11,13 @@ status_pec241 <- fetch_status_proposicao(pec_241_id)
 colnames_pec241 <- c("id","uri","siglaTipo","idTipo","numero","ano","ementa",
                      "dataApresentacao","tipoAutor","idTipoAutor","descricaoTipo","keywords")
 
-colnames_status_pec241 <- c("id","dataHora","sequencia","siglaOrgao","regime","descricaoTramitacao",
+colnames_status_pec241 <- c("id","dataHora","sequencia","siglaOrgao","uriOrgao","regime","descricaoTramitacao",
                             "idTipoTramitacao","descricaoSituacao","idSituacao","despacho","url")
 
 tipos_pec241 <- c("integer","character","character","integer","integer","integer","character",
                   "character","character","integer","character","character")
 
-tipos_status_pec241 <- c("integer","character","integer","character","character","character",
+tipos_status_pec241 <- c("integer","character","integer","character","character","character","character",
                          "character","character","integer","character","character")
 
 names(tipos_pec241) <- colnames_pec241
@@ -31,7 +31,7 @@ test_that("Is dataframe", {
 
 test_that("Dimensoes do dataframe",{
   expect_equal(dim(pec_241), c(1, 12))
-  expect_equal(dim(status_pec241), c(1, 11))
+  expect_equal(dim(status_pec241), c(1, 12))
 })
 
 test_that("Atributos do dataframe",{
