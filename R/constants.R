04a816f2845b9b37808bc17323c359b9ac99a3fa
Tarciso Oliveira <tarcisocomp@gmail.com>
2019-01-24 13:15:18 -0300

    Adiciona constantes com links/caminhos para baixar informações das sessões na Câmara e no Senado.

--- R/constants.R
+++ R/constants.R
@@ -2,18 +2,25 @@
 .CAMARA_API_LINK <- "https://dadosabertos.camara.leg.br"
 .SENADO_API_LINK <- "http://legis.senado.leg.br"
 
+# Links Alternativos
+.CAMARA_WEBSITE_LINK <- "http://www.camara.gov.br"
+
 # Mensagens de erro
 .ERRO_RETORNO_JSON <- "API did not return json"
 .WARNING_PROPOSICAO_ID <- "Pode haver campos incorretos na proposicao. Verifique o tipo, numero e ano."
 .WARNING_SIGLA_PARTIDO <- "Algumas siglas podem estar incorretas."
 
-# Paths
+# Paths Câmara
 .PROPOSICOES_PATH <- "/api/v2/proposicoes"
 .VOTACOES_PATH <- "/api/v2/votacoes"
 .DEPUTADOS_PATH <- "/api/v2/deputados"
 .PARTIDOS_PATH <- "/api/v2/partidos"
 .TIPOS_PROPOSICOES_PATH <- "/api/v2/referencias/tiposProposicao"
+.CAMARA_SESSOES_PATH <- "/proposicoesWeb/sessoes_e_reunioes"
 
+# Paths Senado
+.SENADO_SESSOES_PATH <- "/dadosabertos/materia/ordia/"
+  
 # Link do repositório do rcongresso
 .RCONGRESSO_LINK <- "https://github.com/analytics-ufcg/rcongresso"
 
