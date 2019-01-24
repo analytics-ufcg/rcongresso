da7830680acf2245c5d328bbb798b0f5f57303f0
thayannevls <thayannevls@gmail.com>
2019-01-24 09:24:11 -0300

    export e documenta funcao

--- R/comissoes.R
+++ R/comissoes.R
@@ -2,6 +2,7 @@
 #' @description Fetches a dataframe containing information about the comissao requested
 #' @param sigla Comissao's id
 #' @return Returns a dataframe containing information about the comissao requested
+#' @rdname fetch_comissao_camara
 #' @export
 fetch_comissao_camara <- function(sigla = NULL) {
   parametros <- as.list(environment(), all = TRUE)
