228d35bf2b5b416e4ae2c25ec9b9a099dd8b7656
AndresMRM <andres@inventati.org>
2019-01-28 09:28:01 -0200

    avoid :::

--- R/orgaos.R
+++ R/orgaos.R
@@ -2,7 +2,7 @@
 #' @description Retorna um dataframe contendo os órgãos da câmara
 #' @return Dataframe contendo os órgãos da Câmara
 fetch_orgaos_camara <- function() {
-  rcongresso:::.camara_api(.ORGAOS_FILE_CAMARA_PATH) %>%
+  .camara_api(.ORGAOS_FILE_CAMARA_PATH) %>%
     tibble::as.tibble() %>%
     dplyr::rename(c(idTipoOrgao="tipo_orgao_id", nome="descricao"))
 }
