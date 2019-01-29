ab6190f021ce598d00c4cd61dbe4b7aa4079a589
AndresMRM <andres@inventati.org>
2019-01-29 13:33:10 -0200

    verifica estratura dos dados dos órgãos retornados

--- tests/testthat/test_orgaos.R
+++ tests/testthat/test_orgaos.R
@@ -2,6 +2,7 @@ context("Orgãos")
 
 setup <- function(){
   orgaos_camara <<- fetch_orgaos_camara()
+  comissao_capadr <<- fetch_orgao_camara("CAPADR")
 
   return(TRUE)
 }
@@ -15,13 +16,20 @@ check_api <- function(){
 }
 
 test <- function(){
-  # Testes
   test_that("Is dataframe", {
     expect_true(is.data.frame(orgaos_camara))
   })
 
   test_that("Have many rows", {
     expect_true(nrow(orgaos_camara) >= 5000)
+  })
+
+  test_that("Is dataframe", {
+    expect_true(is.data.frame(comissao_capadr))
+  })
+
+  test_that("Not Empty", {
+    expect_true(nrow(comissao_capadr) != 0)
   })
 }
 
