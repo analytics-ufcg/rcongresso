81b903081e2c5628763b5f0cd437ed22385ff0ce
AndresMRM <andres@inventati.org>
2019-01-31 12:42:57 -0200

    nova estrutura para a cache

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 if (getRversion() >= "2.15.1")  utils::globalVariables(".")
 
-#' Used to store HTTP responses.
-.HTTP_CACHE <- NULL
+.HTTP_CACHE <- new.env(parent = emptyenv())
+.cache_file_path <- usethis::proj_path(fs::path("data", "test_cache.rds"))
 
 #' Extracts the JSON data from an HTTP response
 #' @param response The HTTP response
@@ -70,40 +70,40 @@ if (getRversion() >= "2.15.1")  utils::globalVariables(".")
 
 #' Save the cache to a file
 .save_cache <- function() {
-  pkgenv <- parent.frame()
-  .HTTP_CACHE <- get(".HTTP_CACHE", envir=pkgenv)
-  usethis::use_data(.HTTP_CACHE, internal=T, overwrite=T)
+    saveRDS(.HTTP_CACHE, .cache_file_path)
 }
 
 #' Stores a value in the cache.
 #' @param key Key to store
 #' @param value Value to store
 .put_in_cache <- function(key, value) {
-  pkgenv <- parent.frame()
-  cache <- get(".HTTP_CACHE", envir=pkgenv)
-  cache[[key]] <- value
-  assign(".HTTP_CACHE", cache, envir=pkgenv)
-  .save_cache()
+    if (typeof(.HTTP_CACHE) == "environment") {
+      assign(key, value, envir=.HTTP_CACHE)
+      .save_cache()
+    }
 }
 
 #' Gets a value from the cache.
 #' @param key Key to get
 .get_from_cache <- function(key) {
-  pkgenv <- parent.frame()
-  cache <- get(".HTTP_CACHE", envir=pkgenv)
-  if (is.null(cache)) {
-    tryCatch({
-      load(file=usethis::proj_path(fs::path("R", "sysdata.rda")))
-      assign(".HTTP_CACHE", .HTTP_CACHE, envir=pkgenv)
-      cache <- get(".HTTP_CACHE", envir=pkgenv)
-    }, error = function(error_condition) {
-      print("Initializing cache")
-      assign(".HTTP_CACHE", list(), envir=pkgenv)
-      
-      .save_cache()
-    })
-  }
-  resp <- cache[[key]]
+    if (length(.HTTP_CACHE) == 0) {
+        tryCatch({
+            cache <- readRDS(.cache_file_path)
+            for( key in names(cache) ) assign(key, cache[[key]], envir=.HTTP_CACHE)
+        }, error = function(error_condition) {
+        })
+    }
+    if (typeof(.HTTP_CACHE) == "environment") {
+        # Sometimes it's an environment
+        tryCatch({
+            get(key, envir=.HTTP_CACHE)
+        }, error = function(e) {
+            NULL
+        })
+    } else {
+        # Sometimes it's a list (when checking package)
+        .HTTP_CACHE[[key]]
+    }
 }
 
 .get_from_api <- function(api_base=NULL, path=NULL, query=NULL, timeout = 1, tentativa = 0){
