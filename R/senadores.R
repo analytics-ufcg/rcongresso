e82773f00c3fdaa8d10698461985bedf6fec9528
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-06-05 09:50:27 -0300

    captura ufeleitoral

--- R/senadores.R
+++ R/senadores.R
@@ -4,24 +4,40 @@ fetch_legislatura <- function(legis_initial, legis_final) {
 
   senator_ids <-
     senator_data %>%
-    magrittr::extract2("Parlamentar") %>%
-    tibble::tibble()
+    magrittr::extract2("Parlamentar")
 
   mandatos <-
-    senator_data %>%
-    magrittr::extract2("Parlamentar") %>%
-    magrittr::extract2("Mandatos")
+    senator_ids %>%
+    dplyr::select("Mandatos.Mandato") %>%
+    dplyr::transmute(mandato = Mandatos.Mandato)
+
+  #a <- mandatos$mandato[[2]]$UfParlamentar
+
+  #mandatos <-
+  #  dplyr::mutate(uf = purrr::map(mandatos$mandato[[.auxiliary_cont(0)]]$UfParlamentar, ~.auxiliary_ufParlamentar(.x)))
+
+  #a <-
+  #  mandatos %>%
+  #  dplyr::mutate(uf = purrr::map(numero_da_coluna, ~))
 
   legislatura <-
     senator_ids %>%
+    dplyr::mutate(casa = 'senado') %>%
     dplyr::select(id_parlamentar = IdentificacaoParlamentar.CodigoParlamentar,
                     nome_eleitoral = IdentificacaoParlamentar.NomeParlamentar,
                     nome_completo = IdentificacaoParlamentar.NomeCompletoParlamentar,
                     genero = IdentificacaoParlamentar.SexoParlamentar,
-                    casa = 'senado',
                     partido = IdentificacaoParlamentar.SiglaPartidoParlamentar,
                     uf = IdentificacaoParlamentar.UfParlamentar)
 
-
   return(legislatura)
 }
+
+#.auxiliary_ufParlamentar <- function(ufParlamentar) {
+#  return(ufParlamentar)
+#}
+
+#.auxiliary_cont <- function(cont) {
+#  cont = cont + 1
+#  return(cont)
+#}
