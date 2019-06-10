1bc29e33e750ef9b13c4e54a067fa8acc64d7260
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-06-10 15:14:18 -0300

    adiciona teste

--- tests/testthat/test_senadores.R
+++ tests/testthat/test_senadores.R
@@ -3,8 +3,13 @@ context("Senadores")
 #Setup
 legislatura_inicial <- 40
 legislatura_final <- 56
+
 senadores <- fetch_senadores(legislatura_inicial, legislatura_final)
 
 test_that("fetch_senadores(legis_inicial, legis_final)", {
   expect_true(is.data.frame(senadores))
+})
+
+test_that("fetch_senadores(legis_inicial, legis_final)", {
+  expect_error(fetch_senadores(-1, -1))
 })
