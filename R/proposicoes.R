d7a1807db5eab327c831d23ea8fd6b8fec00ab38
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-06-27 15:08:37 -0300

    adiciona função de autores para ambas as casas

--- R/proposicoes.R
+++ R/proposicoes.R
@@ -249,7 +249,7 @@ fetch_textos_proposicao <- function(id) {
 #' @examples
 #' \dontrun{
 #' relacionadas_pec241 <- fetch_relacionadas("camara",2088351)
-#' } 
+#' }
 #' @seealso
 #'   \code{\link[rcongresso]{fetch_id_proposicao_camara}}
 #' @rdname fetch_relacionadas_camara
@@ -414,6 +414,20 @@ fetch_autor_camara <- function (proposicao_id = NULL) {
 
 }
 
+fetch_autores <- function(proposicao_id = NULL, casa) {
+  if (casa == "camara") {
+    .fetch_autores_camara(proposicao_id)
+  } else if (casa == "senado") {
+    .fetch_autores_senado(proposicao_id)
+  } else {
+    return("Parâmetro 'casa' não identificado.")
+  }
+}
+
+.fetch_autores_senado <- function(proposicao_id) {
+
+}
+
 #' @title Fetches proposition's authors
 #' @description Fetches a dataframe containing basic information about the authors of the proposition
 #' @param proposicao_id Proposition's ID
@@ -421,29 +435,29 @@ fetch_autor_camara <- function (proposicao_id = NULL) {
 #' @return A dataframe containing the basic information about the authors of the proposition
 #' @examples
 #' \dontrun{
-#' fetch_autores_camara(2121442)
-#' } 
+#' .fetch_autores_camara(2121442)
+#' }
 #' @export
-fetch_autores_camara <- function (proposicao_id = NULL, sigla_tipo = "" ) {
+.fetch_autores_camara <- function (proposicao_id = NULL, sigla_tipo = "" ) {
   autor_uri <- paste0(.CAMARA_PROPOSICOES_PATH, '/', proposicao_id, "/autores")
   autores_info <- .camara_api(autor_uri) %>%
     dplyr::rowwise() %>%
     dplyr::mutate(id_autor = dplyr::if_else(!is.na(uri),
-                                            stringr::str_split(uri, '/')[[1]] %>% 
+                                            stringr::str_split(uri, '/')[[1]] %>%
                                               dplyr::last() %>%
                                               as.numeric(),-1),
                   uri = dplyr::if_else(!is.na(uri), as.character(uri), "")) %>%
-    dplyr::ungroup() %>% 
+    dplyr::ungroup() %>%
     dplyr::select(id_autor, nome, cod_tipo = codTipo, tipo, uri)
   if(sigla_tipo %in% c("EMC","PEC")){
-    scrap_df <- tibble::tibble(nome = scrap_autores_from_website(proposicao_id)) %>% 
+    scrap_df <- tibble::tibble(nome = scrap_autores_from_website(proposicao_id)) %>%
       tidyr::separate_rows(nome, sep=", ") %>%
-      tidyr::separate(nome, c("nome","partido_uf"), sep=' - ', extra = "drop", fill = "right") %>% 
+      tidyr::separate(nome, c("nome","partido_uf"), sep=' - ', extra = "drop", fill = "right") %>%
       dplyr::select(-partido_uf)
     autores_info <- autores_info %>%
       dplyr::inner_join(scrap_df, by = "nome")
   }
-  
+
   return(autores_info)
 }
 
@@ -453,16 +467,16 @@ fetch_autores_camara <- function (proposicao_id = NULL, sigla_tipo = "" ) {
 #' @return String with authors names separated by comma
 #' @export
 scrap_autores_from_website <- function(id_prop) {
-  autores_prop_text <- 
+  autores_prop_text <-
     .get_from_url(paste0(.CAMARA_WEBSITE_LINK_2, .AUTORES_CAMARA_PATH, "?idProposicao=", id_prop))%>%
     httr::content('text', encoding = 'utf-8') %>%
     xml2::read_html()  %>%
-    rvest::html_nodes('#content') %>% 
-    rvest::html_nodes('span') %>% 
+    rvest::html_nodes('#content') %>%
+    rvest::html_nodes('span') %>%
     rvest::html_text()
   Sys.sleep(2)
-  
-  paste0(autores_prop_text[3:length(autores_prop_text)], collapse = ", ")  
+
+  paste0(autores_prop_text[3:length(autores_prop_text)], collapse = ", ")
 }
 
 
