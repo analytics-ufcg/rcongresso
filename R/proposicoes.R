c70017a7f3e684bdc8e74d1f2c90b33096058ccb
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-07-03 11:09:31 -0300

    adiciona msg

--- R/proposicoes.R
+++ R/proposicoes.R
@@ -266,8 +266,7 @@ fetch_ids_relacionadas <- function(id, casa) {
   } else if (casa == "senado") {
    relacionadas <- .fetch_relacionadas_senado(id)
    if (nrow(relacionadas) == 0) {
-     return(tibble::tibble())
-     #return("A proposição não possui documentos relacionados.")
+     return("A proposição não possui documentos relacionados.")
    } else {
      relacionadas <- relacionadas %>%
        dplyr::mutate(id_prop = id,
