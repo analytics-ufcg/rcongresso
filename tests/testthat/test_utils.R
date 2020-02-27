3649befff3ae502f7c7a82595b67d5db4ee454bf
Tarciso Braz <tarcisocomp@gmail.com>
2020-02-27 11:59:52 -0300

    Adiciona testes para a função .unnest_df_column

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -51,3 +51,28 @@ test_that(".get_with_exponential_backoff_cached returns json when accept_json is
                                        accept_json=TRUE)
   expect_true(httr::http_type(prop_data_resp) == "application/json")
 })
+
+test_that(".unnest_df_column returns correct result", {
+  bill_id <- 102721
+  json_emendas <- .senado_api(path = paste0(.SENADO_PATH, .EMENDAS_SENADO_PATH, bill_id), asList = TRUE)
+  
+  base_emendas_cols <- c("CodigoEmenda", "NumeroEmenda", "DataApresentacao", 
+                         "ColegiadoApresentacao", "DescricaoTurno", "DescricaoTipoEmenda")
+  
+  emendas_raw_df <- 
+    json_emendas %>%
+    magrittr::extract2("EmendaMateria") %>%
+    magrittr::extract2("Materia") %>%
+    magrittr::extract2("Emendas") %>%
+    magrittr::extract2("Emenda")
+  
+  textos_emendas <- .unnest_df_column(emendas_raw_df,base_emendas_cols,'TextosEmenda.TextoEmenda')
+  autoria_emendas <- .unnest_df_column(emendas_raw_df,base_emendas_cols,'AutoriaEmenda.Autor')
+  decisao_emendas <-.unnest_df_column(emendas_raw_df,base_emendas_cols,'Decisoes.Decisao')
+  
+  expect_true(nrow(textos_emendas) == nrow(emendas_raw_df))
+  expect_true(nrow(autoria_emendas) == nrow(emendas_raw_df))
+  expect_true(nrow(decisao_emendas) == nrow(emendas_raw_df))
+  
+  expect_equal(stringr::str_count(decisao_emendas$Descricao, ';'), as.integer(c(3,3)))
+})
